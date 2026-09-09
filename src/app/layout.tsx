02268670ae4639fefcdace9d0f56e374bd96dd75
Abdallah Atef <bdallhalbnt@email.com>
2026-09-09 17:23:24 +0300

    remove varcel analyses

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { FlickeringGrid } from "@/components/magicui/flickering-grid";
 import SmoothScroll from "@/components/smooth-scroll";
-import { Analytics } from "@vercel/analytics/next";
 
 const geist = Geist({
   subsets: ["latin"],
@@ -73,7 +72,6 @@ export default function RootLayout({
       >
         <ThemeProvider attribute="class" defaultTheme="light">
           <TooltipProvider delayDuration={0}>
-            <Analytics />
             <SmoothScroll>
               <div className="absolute inset-0 top-0 left-0 right-0 h-[100px] overflow-hidden z-0">
                 <FlickeringGrid
